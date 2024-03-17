40406b6b5caad9a4d4d1a2eb991d2e4038e13bfd
Keita1Mori <s20c1118nf@s.chibakoudai.jp>
2024-03-17 16:31:56 +0900

    0317_manipulation_control

--- orne_or_manipulation/orne_or_manipulator_driver/scripts/manipulator_pose_planner.py
+++ orne_or_manipulation/orne_or_manipulator_driver/scripts/manipulator_pose_planner.py
@@ -19,7 +19,7 @@ def main():
     move_group = moveit_commander.MoveGroupCommander("left_arm")
 
     pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.position = Vector3(0.6, 0.2, 0.6)
+    pose_goal.position = Vector3(0.23, 0.27, 0.54)
     q = tf.transformations.quaternion_from_euler(pi, 0, 0)
     pose_goal.orientation = Quaternion(x= q[0], y=q[1], z=q[2], w=q[3])
     move_group.set_pose_target(pose_goal)
