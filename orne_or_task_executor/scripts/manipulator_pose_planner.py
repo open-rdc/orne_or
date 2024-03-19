ac759a4be435741908901ead4d8da02440217358
HarukiOgawa1 <s20c1028uk@s.chibakoudai.jp>
2024-03-19 18:15:37 +0900

    update

--- orne_or_task_executor/scripts/manipulator_pose_planner.py
+++ orne_or_task_executor/scripts/manipulator_pose_planner.py
@@ -14,16 +14,16 @@ from geometry_msgs.msg import Quaternion, Vector3
 def main():
     moveit_commander.roscpp_initialize(sys.argv)
 
-    # rospy.init_node("/object_position")
-    rospy.init_node("manipulator_pose_planner")
+    rospy.init_node("/tf_object_position")
+    rospy.init_node("/manipulator_pose_planner")
 
     move_group = moveit_commander.MoveGroupCommander("left_arm")
-    # pose_goal_sub = rospy.Subscriber("/object_position", PointStamped, callback)
-    # print(pose_goal_sub)
+    pose_goal_sub = rospy.Subscriber("/tf_object_position", PointStamped, callback)
+    print(pose_goal_sub)
 
     pose_goal = geometry_msgs.msg.Pose()
-    # pose_goal.position = Vector3(pose_goal_sub)
-    pose_goal.position = Vector3(0.23, 0.27, 0.0)
+    pose_goal.position = Vector3(pose_goal_sub)
+    # pose_goal.position = Vector3(0.23, 0.27, 0.0)
     q = tf.transformations.quaternion_from_euler(pi, 0, 0)
     pose_goal.orientation = Quaternion(x= q[0], y=q[1], z=q[2], w=q[3])
     move_group.set_pose_target(pose_goal)
