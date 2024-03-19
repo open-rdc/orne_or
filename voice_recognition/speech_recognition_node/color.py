51b2ecc59a5fe79e0f7b639f4473aa7e90f158ff
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-03-19 12:49:20 +0900

    change topic name

--- voice_recognition/speech_recognition_node/color.py
+++ voice_recognition/speech_recognition_node/color.py
@@ -3,59 +3,63 @@
 import rospy
 from std_msgs.msg import String
 
-# グローバル変数としてボールの色とスタートフラグを定義
+# グローバル変数としてボールの色、スタートフラグ、およびパブリッシャーを定義
 ball_color = ""
 start_flag = 0
+color_publisher = None  # パブリッシャーのグローバル変数
 
 def start_callback(data):
     global start_flag
-    # start.pyからのメッセージに応じてスタートフラグを更新
     if data.data == "1":
         start_flag = 1
 
 def color_callback(data):
     global ball_color, start_flag
-    # スタートが検知されていない場合は処理をスキップ
     if start_flag == 0:
         rospy.loginfo("スタートしていません。")
         return
     
-    # ボールか的の色を検知したときに呼ばれる関数
     if "ボール" in data.data or "的" in data.data:
         rospy.loginfo("ボールの色を検知しました。")
         update_colors(data.data)
 
 def update_colors(text):
     global ball_color
+    # 既に色が設定されている場合は更新しない
+    if ball_color:
+        return
+
     characters = [char for char in text]
     i = 0
     while characters[i:i+3] != ["ボ", "ー", "ル"]:
-        if characters[i] == "赤":
-            ball_color = "red"
-        elif characters[i] == "青":
-            ball_color = "blue"
-        elif characters[i] == "黄":
-            ball_color = "yellow"
+        if characters[i] in ["赤", "青", "黄"]:
+            if characters[i] == "赤":
+                ball_color = "red"
+            elif characters[i] == "青":
+                ball_color = "blue"
+            elif characters[i] == "黄":
+                ball_color = "yellow"
+            rospy.loginfo("ボールの色が決定されました： %s", ball_color)
+            break
         i += 1
-    
-    rospy.loginfo("ボールの色： %s", ball_color)
-    # 色の値をパブリッシュする
-    publish_color_value(ball_color)
 
-def publish_color_value(color):
-    # ボールの色を/ball_colorトピックにパブリッシュする
-    pub = rospy.Publisher('/ball_color', String, queue_size=10)
-    rospy.sleep(1)  # パブリッシャがセットアップされるのを待つ
-    pub.publish(color)
+def publish_color_event(event):
+    global ball_color
+    if ball_color:  # ball_colorが設定されていればパブリッシュ
+        color_publisher.publish(ball_color)
 
 def color_detector():
+    global color_publisher
     rospy.init_node('color_detector', anonymous=True)
     
-    # start.pyからのトピックを受信するコールバック関数を登録
+    # パブリッシャーを初期化
+    color_publisher = rospy.Publisher('/ball_color', String, queue_size=10)
+
     rospy.Subscriber("/start_flag", String, start_callback)
-    
-    # 色を受信するトピックのコールバック関数を登録
     rospy.Subscriber("/speech_recog_result", String, color_callback)
+
+    # 1秒ごとに現在のball_colorをパブリッシュする
+    rospy.Timer(rospy.Duration(1), publish_color_event)
     
     rospy.spin()
 
