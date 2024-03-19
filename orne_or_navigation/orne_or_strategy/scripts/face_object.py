2f7b4447a74d3aa2e49ceea7952a1b2ade68864a
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-03-19 13:22:01 +0900

    face ball

--- orne_or_navigation/orne_or_strategy/scripts/face_object.py
+++ orne_or_navigation/orne_or_strategy/scripts/face_object.py
@@ -1,42 +1,56 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
+import std_msgs.msg
 from yolov5_pytorch_ros.msg import BoundingBoxes
 
 class RobotController:
     def __init__(self):
         rospy.init_node('robot_controller', anonymous=True)
-        # cmd_velパブリッシャーの設定
+        rospy.loginfo("start")
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        # カメラ画像の幅を仮定する（実際にはカメラから取得する）
-        self.camera_image_width = 640  # この値は使用しているカメラによって変わります
-        # それぞれの色のトピックに対するサブスクライバーを設定
-        self.sub_red = rospy.Subscriber('/detected_objects/red', BoundingBoxes, self.callback, callback_args='red')
-        self.sub_blue = rospy.Subscriber('/detected_objects/blue', BoundingBoxes, self.callback, callback_args='blue')
-        self.sub_yellow = rospy.Subscriber('/detected_objects/yellow', BoundingBoxes, self.callback, callback_args='yellow')
+        self.camera_image_width = 640
 
-    def callback(self, data, color):
-        rospy.loginfo(f"{color.capitalize()} object detected with bounding boxes: {data.bounding_boxes}")
-        self.align_robot_with_object(data, color)
+        self.selected_color = None  # 初期状態で色が選択されていないことを示す
 
-    def align_robot_with_object(self, detection_msg, color):
-        target_object = f"ball_{color}"
-        for box in detection_msg.bounding_boxes:
-            if box.Class == target_object:
-                center_x = (box.xmin + box.xmax) / 2
-                error_x = center_x - (self.camera_image_width / 2)
-                # 追加: バウンディングボックスの中心座標とカメラ座標の中心との差を表示
-                rospy.loginfo(f"{color.capitalize()} object center X: {center_x}, Error X: {error_x}")
-                twist_msg = Twist()
-                twist_msg.angular.z = -error_x * 0.005
-                self.cmd_vel_pub.publish(twist_msg)
-                rospy.loginfo(f"Aligning robot with {color} object: angular.z = {twist_msg.angular.z}")
-                break
+        # 検出結果を受け取るサブスクライバー
+        self.sub_detection = rospy.Subscriber('/detected_objects_in_image', BoundingBoxes, self.detection_callback)
 
+        # 色に関する指示を受け取るサブスクライバーを/ball_colorに変更
+        self.sub_color = rospy.Subscriber('/ball_color', std_msgs.msg.String, self.color_callback)
+
+    # 新しい色指示用のコールバック
+        # 新しい色指示用のコールバック
+    def color_callback(self, msg):
+        color = msg.data  # 受け取ったメッセージから色を取得
+        rospy.loginfo(f"Received ball color selection: {color}")  # 選択された色を表示
+        self.selected_color = color
+
+    def detection_callback(self, detection_msg):
+        rospy.loginfo(f"Detection callback invoked with selected color: {self.selected_color}")
+        if self.selected_color:
+            for bbox in detection_msg.bounding_boxes:
+                rospy.loginfo(f"Detected object class: {bbox.Class}")  # バウンディングボックスのクラス名を表示
+                if f"ball_{self.selected_color}" == bbox.Class:
+                    self.process_detected_object(bbox, self.selected_color)
+                else:
+                    rospy.loginfo(f"Detected object class does not match selected color: {bbox.Class}")
+        else:
+            rospy.loginfo("No ball color selected.")
+
+    def process_detected_object(self, bbox, color):
+        
+        center_x = (bbox.xmin + bbox.xmax) / 2
+        error_x = center_x - (self.camera_image_width / 2)
+        rospy.loginfo(f"{color.capitalize()} object center X: {center_x}, Error X: {error_x}")
+        twist_msg = Twist()
+        twist_msg.angular.z = -error_x * 0.005
+        self.cmd_vel_pub.publish(twist_msg)
+        rospy.loginfo(f"Aligning robot with {color} object: angular.z = {twist_msg.angular.z}")
 
 if __name__ == '__main__':
     try:
         controller = RobotController()
-        rospy.spin()  # Keep the program running until a shutdown signal is received
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
