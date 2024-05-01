a10e1a4a1febead7b4efa502f3f5c3b30929f0e1
MibuchiYuta <6343yuuta@gmail.com>
2024-05-01 17:35:30 +0900

    update door script

--- orne_or_task_executor/scripts/door.py
+++ orne_or_task_executor/scripts/door.py
@@ -20,25 +20,42 @@ def main():
 
     # ホームポジション
     right_joint_goal = {
-        'r_sholder_roll_joint': -pi / 2.001,
+        'r_sholder_roll_joint': -89 * pi / 180,
         'r_sholder_pitch_joint': 0,
-        'r_elbow_roll_joint': pi * 16.95 / 18,
-        'r_wrist_roll_joint': 0,
-        'r_wrist_pitch_joint': 0,
+        'r_elbow_roll_joint': 169 * pi / 180,
+        'r_wrist_roll_joint': 11 * pi / 180,
+        'r_wrist_pitch_joint': -89 * pi / 180,
         'r_wrist_yaw_joint': 0
     }
     right_arm.set_joint_value_target(right_joint_goal)
     right_arm.go(wait=True)
+    
     left_joint_goal = {
-        'l_sholder_roll_joint': -pi / 2.001,
+        'l_sholder_roll_joint': -89 * pi / 180,
         'l_sholder_pitch_joint': 0,
-        'l_elbow_roll_joint': pi * 16.95 / 18,
-        'l_wrist_roll_joint': 0,
-        'l_wrist_pitch_joint': 0,
+        'l_elbow_roll_joint': 169 * pi / 180,
+        'l_wrist_roll_joint': 11 * pi / 180,
+        'l_wrist_pitch_joint': -89 * pi / 180,
         'l_wrist_yaw_joint': 0
     }
     left_arm.set_joint_value_target(left_joint_goal)
     left_arm.go(wait=True)
+
+    #ready
+    right_wrist_goal = PoseStamped()
+    right_wrist_goal.header.frame_id = "world"
+    right_wrist_goal.pose.position.x = 0.26
+    right_wrist_goal.pose.position.y = 0
+    right_wrist_goal.pose.position.z = 1
+    right_wrist_goal.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(-pi/2, 0, pi/2))
+    
+    right_arm.set_pose_target(right_wrist_goal)
+
+    right_arm.go(wait=True)
+
+    target_joint_positions = [0 * (pi / 180)]
+    right_hand.set_joint_value_target(target_joint_positions)
+    right_hand.go(wait=True)
 
     #ドアノブ上
     right_wrist_goal = PoseStamped()
@@ -52,7 +69,7 @@ def main():
 
     right_arm.go(wait=True)
 
-    target_joint_positions = [10 * (pi / 180)]
+    target_joint_positions = [0 * (pi / 180)]
     right_hand.set_joint_value_target(target_joint_positions)
     right_hand.go(wait=True)
 
@@ -63,7 +80,7 @@ def main():
     right_wrist_goal.pose.position.x = 0.45
     right_wrist_goal.pose.position.y = 0.11
     right_wrist_goal.pose.position.z = 0.8
-    right_wrist_goal.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, 0, pi/2))
+    right_wrist_goal.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, -pi*5/180, pi/2))
     
     right_arm.set_pose_target(right_wrist_goal)
 
@@ -77,9 +94,25 @@ def main():
     right_wrist_goal = PoseStamped()
     right_wrist_goal.header.frame_id = "world"
     right_wrist_goal.pose.position.x = 0.3
-    right_wrist_goal.pose.position.y = -0.2
+    right_wrist_goal.pose.position.y = 0.05
     right_wrist_goal.pose.position.z = 0.8
-    right_wrist_goal.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, 0, pi/2))
+    right_wrist_goal.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, pi*5/180, pi*5/8))
+    
+    right_arm.set_pose_target(right_wrist_goal)
+
+    right_arm.go(wait=True)
+
+    target_joint_positions = [10 * (pi / 180)]
+    right_hand.set_joint_value_target(target_joint_positions)
+    right_hand.go(wait=True)
+
+    #ドアノブ引く
+    right_wrist_goal = PoseStamped()
+    right_wrist_goal.header.frame_id = "world"
+    right_wrist_goal.pose.position.x = 0.17
+    right_wrist_goal.pose.position.y = -0.16
+    right_wrist_goal.pose.position.z = 0.8
+    right_wrist_goal.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, pi*5/180, pi*3/4))
     
     right_arm.set_pose_target(right_wrist_goal)
 
