e63458c707f28894030b42c5d9dc429d68d479ff
KiyoshiroKawanabe <s19C1031UL@s.chibakoudai.jp>
2024-05-04 16:00:57 +0900

    modified

--- orne_or_task_executor/scripts/manipulator_left.py
+++ orne_or_task_executor/scripts/manipulator_left.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# coding:UTF-8
+# coding: UTF-8
 
 import sys
 from math import pi
@@ -8,6 +8,20 @@ import moveit_commander
 import rospy
 import tf
 from geometry_msgs.msg import PoseStamped, Quaternion, Vector3
+
+def callback(tf_object_position, args):
+    left_arm = args[0]
+    try:
+        left_arm_goal = PoseStamped()
+        left_arm_goal.header.frame_id = "world"  # Or the frame_id from tf_object_position if different
+        left_arm_goal.pose.position.x = tf_object_position.position.x
+        left_arm_goal.pose.position.y = tf_object_position.position.y
+        left_arm_goal.pose.position.z = tf_object_position.position.z
+        left_arm_goal.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, 0, pi))
+        left_arm.set_pose_target(left_arm_goal)
+        left_arm.go(wait=True)
+    except rospy.ROSInterruptException:
+        return
 
 def main():
     moveit_commander.roscpp_initialize(sys.argv)
@@ -20,20 +34,10 @@ def main():
     joint_goal[left_hand.get_active_joints().index('l_hand_left_joint')] = -pi / 4
     left_hand.go(joint_goal, wait=True)
 
-    left_arm_goal = PoseStamped()
-    left_arm_goal.header.frame_id = "world"
-    left_arm_goal.pose.position.x = 0.3940
-    left_arm_goal.pose.position.y = 0.1298
-    left_arm_goal.pose.position.z = 0.7405
-    left_arm_goal.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, 0, pi))
-    
-    left_arm.set_pose_target(left_arm_goal)
+    rospy.Subscriber("/tf_object_position", PoseStamped, callback, (left_arm,))
 
-    left_arm.go(wait=True)
+    rospy.spin()
 
-    #target_joint_positions = [10 * (pi / 180)]
-    #right_hand.set_joint_value_target(target_joint_positions)
-    #right_hand.go(wait=True)
-    
 if __name__ == "__main__":
     main()
+
--- orne_or_task_executor/scripts/tf_object_position.py
+++ orne_or_task_executor/scripts/tf_object_position.py
@@ -12,16 +12,15 @@ class PointTransformer:
 
     def callback(self, point_msg):
         try:
-            # カメラ座標系からベース座標系への変換を待つ
-            self.listener.waitForTransform('/base_link', point_msg.header.frame_id, rospy.Time(0), rospy.Duration(4.0))
-            # 座標変換を実行
+            if not self.listener.canTransform('/base_link', point_msg.header.frame_id, rospy.Time(0)):
+                rospy.logwarn("Transformation from %s to /base_link not ready.", point_msg.header.frame_id)
+                return
             transformed_point = self.listener.transformPoint('/base_link', point_msg)
             transformed_point.point.x /= 1000
             transformed_point.point.y /= 1000
             transformed_point.point.z += 1100
             transformed_point.point.z /= 1000
             rospy.loginfo("Transformed Point: %s", transformed_point.point)
-            # 変換されたポイントをパブリッシュ
             self.publisher.publish(transformed_point)
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
             rospy.logerr("TF error: %s", e)
