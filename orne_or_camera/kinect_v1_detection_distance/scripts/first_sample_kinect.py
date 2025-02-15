87141e575edd38784a9431d0110391f47c7efb0e
Riku Yokoo <s19C1123JF@s.chibakoudai.jp>
2025-02-15 20:30:56 +0900

    feat: 青の物体の座標を取得

--- orne_or_camera/kinect_v1_detection_distance/scripts/first_sample_kinect.py
+++ orne_or_camera/kinect_v1_detection_distance/scripts/first_sample_kinect.py
@@ -11,14 +11,14 @@ class KinectNode:
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/kinect/camera/rgb/image_color", Image, self.RGBImageCallback)
         self.depth_sub = rospy.Subscriber("/kinect/camera/depth/image", Image, self.depthCallback)
+        self.blue_object_coordinates = []
 
     def RGBImageCallback(self, rgb_image):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(rgb_image, "bgr8")
+            self.blue_object_coordinates = self.detect_blue_object(cv_image)
         except CvBridgeError as e:
             print(e)
-        cv2.imshow("RGB Image", cv_image)
-        cv2.waitKey(3)
 
     def depthCallback(self, depth_image):
         try:
@@ -27,6 +27,20 @@ class KinectNode:
             print(e)
         cv2.imshow("Depth Image", cv_image)
         cv2.waitKey(3)
+
+    def detect_blue_object(self, image):
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower_blue = np.array([110, 50, 50])
+        upper_blue = np.array([130, 255, 255])
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        res = cv2.bitwise_and(image, image, mask=mask)
+        cv2.imshow("Blue Object", res)
+        cv2.waitKey(3)
+        return mask
+
+    def calculate_distance(self, depth_image, coordinates):
+        pass
+
 
 def main():
     rospy.init_node("kinect_node")
