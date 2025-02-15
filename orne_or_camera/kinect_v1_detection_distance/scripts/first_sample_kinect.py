6ff98477eb951b24c2276f23703116d6a410469c
Riku Yokoo <s19C1123JF@s.chibakoudai.jp>
2025-02-15 18:03:33 +0900

    study: Kinectのトピックを受け取り表示するためのサンプル

---
+++ orne_or_camera/kinect_v1_detection_distance/scripts/first_sample_kinect.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+#class KinectV1DetectDistance:
+class KinectNode:
+    def __init__(self):
+        self.bridge = CvBridge
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_color", Image, self.RGBImageCallback)
+        self.depth_sub = rospy.Subscriber("/camera/depth/image", Image, self.depthCallback)
+
+    def RGBImageCallback(self, rgb_image):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(rgb_image, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+        cv2.imshow("RGB Image", cv_image)
+        cv2.waitKey(3)
+
+    def depthCallback(self, depth_image):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(depth_image, "32FC1")
+        except CvBridgeError as e:
+            print(e)
+        cv2.imshow("Depth Image", cv_image)
+        cv2.waitKey(3)
+
+def main():
+    rospy.init_node("kinect_node")
+    kinect_node = KinectNode()
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
+
